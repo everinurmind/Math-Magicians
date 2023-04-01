027e964ce09c4d4ebcc0a94ecb50c9534cb17772
everinurmind <nurbol.sultanov@gmail.com>
2023-03-31 23:03:26 -0600

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
-import FetchQuotes from './components/FetchApi';
+import Quote from './components/FetchApi';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
-      <FetchQuotes />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
     </div>
   );
 }
