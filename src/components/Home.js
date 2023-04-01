bb96f327147dd3a1c090069f1bfb72d907c57f09
everinurmind <nurbol.sultanov@gmail.com>
2023-03-31 23:04:17 -0600

    Add pages and header

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import './Home.css';
+
+const Home = () => (
+  <div className="site-home">
+    <h2 id="site-header">Welcome to our page!</h2>
+    <section className="text-section">
+      <p>
+        Welcome to our React app! Our app features a navigation menu with three main pages:
+        Home, Calculator, and Quotes.
+      </p>
+      <p>
+        The Calculator page is a simple yet powerful calculator.
+        Whether you need to add, subtract, multiply, or divide, our calculator has got you covered.
+      </p>
+      <p>
+        The Quotes page is perfect for when you need a little bit of inspiration or motivation.
+        With just a click of a button, you can generate a random quote.
+      </p>
+    </section>
+  </div>
+);
+
+export default Home;
